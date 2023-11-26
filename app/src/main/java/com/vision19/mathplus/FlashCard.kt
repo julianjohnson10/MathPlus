2874d70f81ecebc26e79fa9fe447ca4b0dc04d36
julian.johnson.10 <julian.johnson.10@outlook.com>
2023-11-25 23:27:40 -0800

    updated flashcards

--- app/src/main/java/com/vision19/mathplus/FlashCard.kt
+++ app/src/main/java/com/vision19/mathplus/FlashCard.kt
@@ -1,13 +1,22 @@
 package com.vision19.mathplus
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlin.random.Random
@@ -23,96 +32,106 @@ fun FlashCard(operation: String, userAnswerViewModel: UserAnswerViewModel) {
     var countdownSeconds by remember { mutableIntStateOf(0) }
     val question = "$num1 ${getOperationSymbol(operation)} $num2"
     val coroutineScope = rememberCoroutineScope()
-
-    Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+    var backgroundColor = Color.Transparent
+    var choice by remember { mutableIntStateOf(0)}
+    Box(
+        modifier = Modifier.fillMaxSize().background(backgroundColor) // Set the background color here
     ) {
-        Spacer(modifier = Modifier.height(30.dp))
+        ConstraintLayout(modifier = Modifier.fillMaxSize()) {
+            // Create references for the composables to constrain
+            val (questionText, answerText, checkMark, answerChoices) = createRefs()
 
-        // Reserve space for the correct answer text
-        if (showAnswer) {
-            Text(
-                text = "$num1 ${getOperationSymbol(operation)} $num2 = $result",
-                style = MaterialTheme.typography.labelLarge,
-                modifier = Modifier.padding(bottom = 30.dp) // Adjust the padding as needed
-            )
-        } else {
-            Text(
-                text = "$num1 ${getOperationSymbol(operation)} $num2 =",
-                style = MaterialTheme.typography.labelLarge,
-            )
-        }
-
-        // Display the countdown timer
-        // If the countdown timer is greater than 0, display the correct or incorrect message
-        if (countdownSeconds > 0) {
-            Text(
-                text = if (correct) "Correct!" else "Incorrect!",
-                style = MaterialTheme.typography.labelMedium,
-                color = if (correct) Color.Green else Color.Red
-            )
-            //Add a green or red checkmark if correct or incorrect
-
-            if (correct) {
-                Text(
-                    text = "✓",
-                    style = MaterialTheme.typography.labelLarge,
-                    color = Color.Green
-                )
+            val answer = if (showAnswer) {
+                "$num1 ${getOperationSymbol(operation)} $num2 = $choice"
             } else {
+                "$num1 ${getOperationSymbol(operation)} $num2 ="
+            }
+            Text(
+                text = answer,
+                style = MaterialTheme.typography.labelLarge,
+                modifier = Modifier.constrainAs(questionText) {
+                    top.linkTo(parent.top, margin = 60.dp)
+                    //                bottom.linkTo(answerText.top, margin = 10.dp)
+                    centerHorizontallyTo(parent)
+                },
+                textAlign = TextAlign.Center
+            )
+
+            if (countdownSeconds > 0) {
+                // Update 'correct' based on the user's choice and the correct answer
+//                val userChoice = choices.find { it == result }
+//                correct = userChoice != null && userChoice == result
+                backgroundColor = Color.Red
                 Text(
-                    text = "✗",
+                    text = if (correct) "Correct!" else "Incorrect!",
                     style = MaterialTheme.typography.labelLarge,
-                    color = Color.Red
+                    color = if (correct) Color.Green else Color.Red,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.constrainAs(answerText) {
+                        top.linkTo(questionText.bottom)
+                        //                    bottom.linkTo(checkMark.top)
+                        centerHorizontallyTo(parent)
+                    },
+                )
+                // Add a green or red checkmark if correct or incorrect
+                Text(
+                    text = if (correct) "✓" else "✗",
+                    style = MaterialTheme.typography.labelLarge,
+                    color = if (correct) Color.Green else Color.Red,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.constrainAs(checkMark) {
+                        top.linkTo(answerText.top)
+                        bottom.linkTo(answerChoices.top)
+                        centerHorizontallyTo(parent)
+                    },
+                )
+            }
+
+            Box(
+                modifier = Modifier.constrainAs(answerChoices) {
+                    //                top.linkTo(checkMark.bottom) // Always below answerText
+                    bottom.linkTo(parent.bottom, margin = 50.dp)
+                    centerHorizontallyTo(parent)
+                }
+            ) {
+                // Replace your Row with the FourButtonGrid
+                MultipleChoices(
+                    choices = choices,
+                    onClick = { selectedChoice ->
+
+                        // Update 'correct' based on the user's choice and the correct answer
+                        correct = selectedChoice == result
+                        choice = selectedChoice
+                        val userAnswer = UserAnswer(
+                            questionType = operation,
+                            question = question,
+                            userAnswer = selectedChoice,
+                            isCorrect = correct
+                        )
+
+                        showAnswer = true // Show the correct answer
+                        countdownSeconds = 1 // Start the countdown timer
+
+                        coroutineScope.launch {
+                            while (countdownSeconds > 0) {
+                                delay(1000) // Wait for 1 second
+                                countdownSeconds-- // Decrement the countdown timer
+                            }
+                            num1 = Random.nextInt(10)
+                            num2 = Random.nextInt(10)
+                            showAnswer = false
+                            result = calculateResult(num1, num2, operation)
+                            choices = generateChoices(result, operation)
+                        }
+
+                        userAnswerViewModel.insertUserAnswer(userAnswer)
+                    }
                 )
             }
         }
-
-        Box(
-            modifier = Modifier
-                .padding(16.dp) // Add padding if needed
-                .width(400.dp) // Set the size of the MultipleChoices composable
-                .offset(y = 16.dp), // Offset from the top
-            contentAlignment = Alignment.Center,
-        ) {
-            // Replace your Row with the FourButtonGrid
-            MultipleChoices(
-                choices = choices,
-                onClick = { selectedChoice ->
-
-                    correct = selectedChoice == result
-
-                    val userAnswer = UserAnswer(
-                        questionType = operation,
-                        question = question,
-                        userAnswer = selectedChoice,
-                        isCorrect = correct
-                    )
-
-
-                    showAnswer = true // Show the correct answer
-                    countdownSeconds = 1 // Start the countdown timer
-
-                    coroutineScope.launch {
-                        while (countdownSeconds > 0) {
-                            delay(1000) // Wait for 1 second
-                            countdownSeconds-- // Decrement the countdown timer
-                        }
-                        num1 = Random.nextInt(10)
-                        num2 = Random.nextInt(10)
-                        showAnswer = false
-                        result = calculateResult(num1, num2, operation)
-                        choices = generateChoices(result, operation)
-                    }
-
-                    userAnswerViewModel.insertUserAnswer(userAnswer)
-
-                }
-            )
-        }
     }
 }
+
 
 fun calculateResult(num1: Int, num2: Int, operation: String): Comparable<*> {
     return when(operation) {
